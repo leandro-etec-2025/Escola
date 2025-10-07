2f2975bf9321af6d23be7c73c5d5e70b551f40d1
leandro-etec-2025 <leandrocesardacruz.cps.2025@gmail.com>
2025-10-07 20:23:54 -0300

    Criado classe alunos

---
+++ app/src/main/java/com/example/myapplication/Alunos.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+public class Alunos {
+
+    String nomeAt;
+    String cursoAt;
+
+    public Alunos(String namePar, String coursePar){
+        this.nomeAt = namePar;
+        this.cursoAt = coursePar;
+    }
+
+    public String getNome(){
+        return nomeAt;
+    }
+
+    public String getCurso(){
+        return cursoAt;
+    }
+
+
+
+
+
+}
